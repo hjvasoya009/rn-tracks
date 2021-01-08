576ea855d0fd16a1fae4411c4707a63f8818e9ff
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2021-01-08 02:27:03 -0800

    Tracker App Section-15 Vid-213

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://2a4842b0b8c7.ngrok.io'
+});
---
+++ src/context/AuthContext.js
@@ -0,0 +1,51 @@
+import { AsyncStorage } from '@react-native-community/async-storage';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
+
+        case 'signup':
+            return { errorMessage: '', token: action.payload };
+
+        default:
+            return state;
+    }
+};
+
+const signup = (dispatch) => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password });
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signup', payload: response.data.token });
+        navigate('TrackList')
+    } catch (err) {
+        console.log(err.response.data);
+        dispatch({ type: 'add_error', payload: 'Something went wrong with signup' })
+    }
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // Try to signin
+
+        // Handle success by updating state
+
+        // Handle failure by showing error message (somehow)
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        // Somehow sign out!!!
+    };
+};
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    { signin, signout, signup },
+    { token: null, errorMessage: '' }
+);
